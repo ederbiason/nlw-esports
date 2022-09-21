054f80af2c3c2f7c645c388c602351c92de2f874
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-09-21 16:31:19 -0300

    Fetch
    Fecth to get all the games in the backend of the project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react'
+
 import './styles/main.css'
 import { GameBanner } from './components/GameBanner'
 
 import logo from './assets/logo-nlw-esports.svg'
 import { CreateAdBanner } from './components/CreateAdBanner'
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number
+  }
+}
+
 export function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
+
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logo} alt="Logo image" />
@@ -14,12 +36,14 @@ export function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={4} />
-        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={2} />
-        <GameBanner bannerUrl="/game-3.png" title="League of Legends" adsCount={4} />
-        <GameBanner bannerUrl="/game-4.png" title="League of Legends" adsCount={4} />
-        <GameBanner bannerUrl="/game-5.png" title="League of Legends" adsCount={4} />
-        <GameBanner bannerUrl="/game-6.png" title="League of Legends" adsCount={4} />
+        {games.map((game) => (
+          <GameBanner
+            key={game.id}
+            bannerUrl={game.bannerUrl}
+            title={game.title}
+            adsCount={game._count.ads}
+          />
+        ))}
       </div>
 
       <CreateAdBanner />
