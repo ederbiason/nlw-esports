31e26c5c6bbca4cd5b019d764c7129553581b87b
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-09-22 15:40:26 -0300

    Game screen
    When the user press a game, will open another screen with the information about it

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,21 +1,58 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { useRoute } from '@react-navigation/native'
+import { useRoute, useNavigation } from '@react-navigation/native'
+import { View, TouchableOpacity, Image } from 'react-native';
+import { Entypo } from '@expo/vector-icons'
 
-import { Background } from '../../components/Background';
+import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { styles } from './styles';
+import { THEME } from '../../theme';
+
 import { GameParams } from '../../@types/navigation';
+import { Heading } from '../../components/Heading';
+import { Background } from '../../components/Background';
 
 export function Game() {
+  const navigation = useNavigation()
+
   const route = useRoute()
   const game = route.params as GameParams
-  console.log(game)
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
 
   return (
     <Background>
       <SafeAreaView style={styles.container}>
-      
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo 
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image 
+            source={logoImg}
+            style={styles.logo}
+          />
+
+          <View style={styles.right} />
+        </View>
+
+        <Image 
+          source={{uri: game.bannerUrl}}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading 
+          title={game.title}
+          subtitle="Conecte-se e comece a jogar!"
+        />
       </SafeAreaView>
     </Background>
   );
