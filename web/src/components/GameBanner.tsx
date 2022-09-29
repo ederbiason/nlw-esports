7ed77318c2e794f0b8ef6083479fb99e107c33e6
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-09-29 15:56:12 -0300

    Fetching and navigate
    Using useNavigate I pass some params to use in the Game component and I did the fetch to get all the ads in the game selected

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router-dom";
 
 interface Props {
     bannerUrl: string;
     title: string;
     adsCount: number;
+    id: string;
 }
 
-export const GameBanner = ({ bannerUrl, title, adsCount}: Props) => {
+export const GameBanner = ({ bannerUrl, title, adsCount, id}: Props) => {
+    const navigate = useNavigate()
+
+    function goToGameAds() {
+        navigate(`/games/${id}`, { state: { title, bannerUrl, id } })
+    }
+
     return (
-        <a href="" className="relative rounded-lg overflow-hidden">
+        <a href="" className="relative rounded-lg overflow-hidden"  onClick={() => goToGameAds()}>
             <img src={bannerUrl} alt="" />
 
             <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
--- web/src/components/Games.tsx
+++ web/src/components/Games.tsx
@@ -1,32 +1,55 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom";
-import { Game } from "../App";
+import { useEffect, useState } from "react";
+import { useLocation, useParams} from "react-router-dom";
+import { DuoCard } from "./DuoCard";
 
-import logo from '../assets/logo-nlw-esports.svg'
+export interface DuoCardProps {
+  hourEnd: string; 
+  hourStart: string;
+  id: string;
+  name: string;
+  useVoiceString: boolean;
+  weeksDays: string[];
+  yearsPlaying: number;
+}
 
 export const Games = () => {
-  const [game, setGame] = useState<Game>()
+  const [duos, setDuos] = useState<DuoCardProps[]>([])
+
+  const location = useLocation();
   const { id } = useParams()
 
   useEffect(() => {
     axios(`http://localhost:3333/games/${id}/ads`)
       .then(response => {
-        setGame(response.data)
+        setDuos(response.data)
       })
   }, [])
 
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-      <img src={logo} alt="Logo image" />
+    <div className="max-w-[1344px] mx-auto my-20">
+      <div className="flex items-center justify-evenly">
+        <img src={location.state.bannerUrl} alt="Logo image" className="rounded-lg h-64" />
 
-      <h1 className="text-6xl text-white font-black mt-20">
-        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui
-      </h1>
+        <div>
+          <h1 className="text-6xl text-white font-black mt-10">
+            Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui
+          </h1>
 
-      <h1>
-        {game?.title}
-      </h1>
+          <h1 className="text-6xl text-white font-black mt-3">
+            {location.state.title}
+          </h1>
+        </div>
+      </div>
+
+      <div>
+        {duos.map((duo) => (
+          <DuoCard
+            key={duo.id}
+            data={duo}
+          />
+        ))}
+      </div>
     </div>
   )
 }
